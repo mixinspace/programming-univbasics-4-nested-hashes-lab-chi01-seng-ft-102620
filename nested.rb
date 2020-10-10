4cf189b5af9337e91f6ed004d8a6f76506baac45
Islam Madkaur <islammadkaur@gmail.com>
2020-10-10 21:39:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,10 +87,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    programmer_hash[:yukihiro_matsumoto => {
+    hash.update(programmer_hash) {:yukihiro_matsumoto => {
       :known_for => "Ruby",
       :languages => ["LISP", "C"]}
-    ]
+    }
 
 end
 
